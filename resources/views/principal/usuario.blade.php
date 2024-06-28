e1cfdb46151bb637bf4e0babf0cfb23f67ef081d
Julguz12345 <julioguzmanortiz97@gmail.com>
2024-06-27 22:56:03 -0500

    Responsive

--- resources/views/principal/usuario.blade.php
+++ resources/views/principal/usuario.blade.php
@@ -142,15 +142,15 @@
                     <div class="card m-auto">
                         <div class="card-body">
                             <center><h5 class="card-title">Factura #{{ $factura->id }}</h5></center>
-                            <p class="card-text">Nombre: {{ $factura->usuario->name }}</p>
-                            <p class="card-text"><strong>Correo:</strong> {{ $factura->correo }}</p>
+                            <p class="card-text"><strong>Fecha:</strong> {{ $factura->created_at }}</p>
+                            <p class="card-text"><strong>Nombre: </strong>{{ $factura->usuario->name }}</p>
                             <p class="card-text"><strong>Dirección:</strong> {{ $factura->direccion }}</p>
                             <p class="card-text"><strong>Código Postal:</strong> {{ $factura->codigo_postal }}</p>
                             <p class="card-text"><strong>Método de Pago:</strong> {{ $factura->metodo_pago }}</p>
                             <p class="card-text"><strong>Producto:</strong> {{ $factura->producto->nombre }}</p>
                             <p class="card-text"><strong>Cantidad:</strong> {{ $factura->cantidad_compra }}</p>
                             <hr>
-                            <p class="card-text"><strong>Total:</strong> ${{ $factura->total }}</p>
+                            <p class="card-text"><strong>Total: ${{ $factura->total }}</strong></p>
                         </div>
                     </div>
                     @endforeach
