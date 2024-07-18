89c1a432cf33c68d5edef0f6ad82ec5cba74a7eb
felipeb-sudo <felipebonilla.luis22gmail.com>
2024-07-18 12:44:34 -0500

    despliegues 10.1

--- app/Http/Controllers/registerController.php
+++ app/Http/Controllers/registerController.php
@@ -144,6 +144,6 @@ class registerController extends Controller
     {
         $elimausu =User::findOrFail($id);
         $elimausu -> delete();
-        return redirect('/administrador');
+        dd($elimausu);
     }
 }
